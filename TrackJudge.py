3ddef07c977741984c479926da0b37d9b799c8d2
Nachokhan <ignacio.rigoni@gmail.com>
2020-04-25 20:27:02 -0300

    (+) New class to calcualte difficulties

---
+++ TrackJudge.py
@@ -0,0 +1,26 @@
+"""
+TrackJudge is the module thah gives an opinion of a track.
+This means, based on different variables, the Jugde will 
+emit a value of difficulty for a segment or a track.
+
+Different variables to consider are:
+    - Distance
+    - Elevation Height
+    - Accumulated Elevation
+    - Slope
+    - Elevation above the see level.
+    - Curves
+
+Other variable to consider, but difficult to mess are:
+    - Type of grund (asphalt, gravel road, rocks)
+    - Lose ground
+    - Obstacles 
+    - Etc.
+"""
+
+
+
+def CalcSegmentDifficulty(segment):
+    return None
+
+
