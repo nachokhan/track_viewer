96fef10ce006285a6d78e2c2f7378f00a5c2e905
Nachokhan <ignacio.rigoni@gmail.com>
2020-04-27 19:39:55 -0300

    (+) Random problems

---
+++ naquever/prob_curvas.py
@@ -0,0 +1,28 @@
+"""
+Problema problem¡atico:
+
+Descripción.
+
+Tenemos un arreglo de puntos en el espacio (bidimensaional) de coordenadas (x,y).
+Esas coordenadas representan puntos consecutivos, o sea, puntos de una ruta.
+La idea es el que tenga huevos de armar una función en python llamada "contar_curvas"
+que reciva como parámetros:
+
+    - Arreglo con los putos x,y
+    - Un angulo mínimo que indica que una curva es una curva.
+
+Y que devuelva la cantidad de curvas que se hacen en el trayecto, la intesidad de las mismas, 
+y las posiciones (puntos x,y) donde comienza cada una.
+
+Si por ejemplo, hay un desvio del trayecto de 5 grados, es muy bajo el cambio de direccion
+para calificarlo como curva. Pero si es de 80, ya estamos hablando de un lindo volantazo.
+
+Entonces, definir la funcion:
+
+def contar_curvas(puntos, min_angulo):
+    # TODO
+
+"""
+
+def contar_curvas(puntos, min_angulo):
+    raise NotImplementedError
