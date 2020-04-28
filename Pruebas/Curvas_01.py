d3b2619f659279fdfdd186deaac129fbfefa4e12
Nachokhan <ignacio.rigoni@gmail.com>
2020-04-28 19:27:24 -0300

    Refactoring (moved every test to Pruebas)

---
+++ Pruebas/Curvas_01.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+
+from Pruebas.prob_curvas import puntos_1 as pts1
+from Pruebas.prob_curvas import puntos_2 as pts2
+from Pruebas.prob_curvas import puntos_3 as pts3
+from Pruebas.prob_curvas import puntos_4 as pts4
+from Pruebas.prob_curvas import contar_curvas as contar
+
+def RevisarCurvas(pts):
+    a,b,c = contar(pts, 45)
+    print("Curvas: ", a)
+    return c
+
+def main():
+
+    fig = plt.figure(figsize=(55, 25))
+    fig.suptitle("CURVES", size="xx-large")
+    grid = plt.GridSpec(2, 2, wspace=0.5, hspace=0.5)
+    ax1 = fig.add_subplot(grid[0, 1])
+    ax2 = fig.add_subplot(grid[1, 1])
+    ax3 = fig.add_subplot(grid[1, 0])
+    ax4 = fig.add_subplot(grid[0, 0])
+    
+    a1 = RevisarCurvas(pts1)
+    a2 = RevisarCurvas(pts2)
+    a3 = RevisarCurvas(pts3)
+    a4 = RevisarCurvas(pts4)
+
+    ejex1 = [(p[0]) for p in pts1]
+    ejey1 = [(p[1]) for p in pts1]
+    ejex2 = [(p[0]) for p in pts2]
+    ejey2 = [(p[1]) for p in pts2]
+    ejex3 = [(p[0]) for p in pts3]
+    ejey3 = [(p[1]) for p in pts3]
+    ejex4 = [(p[0]) for p in pts4]
+    ejey4 = [(p[1]) for p in pts4]
+
+    ax1.plot(ejex1, ejey1, color = "blue")
+    ax2.plot(ejex2, ejey2, color = "blue")
+    ax3.plot(ejex3, ejey3, color = "blue")
+    ax4.plot(ejex4, ejey4, color = "blue")
+
+    scat1x = [(pts1[p][0]) for p in a1]
+    scat1y = [(pts1[p][1]) for p in a1]
+    scat2x = [(pts2[p][0]) for p in a2]
+    scat2y = [(pts2[p][1]) for p in a2]
+    scat3x = [(pts3[p][0]) for p in a3]
+    scat3y = [(pts3[p][1]) for p in a3]
+    scat4x = [(pts4[p][0]) for p in a4]
+    scat4y = [(pts4[p][1]) for p in a4]
+
+    ax1.scatter(scat1x, scat1y, color = "green")
+    ax2.scatter(scat2x, scat2y, color = "green")
+    ax3.scatter(scat3x, scat3y, color = "green")
+    ax4.scatter(scat4x, scat4y, color = "green")
+
+    plt.show()
---
+++ Pruebas/Curvas_02.py
@@ -0,0 +1,87 @@
+from TrackReader import TrackFileReader
+import matplotlib.pyplot as plt
+
+def main(filename, min_degree = 45, scatter_all = False, scatter_curves = True, plot_curves = True):
+    f_showAcc(filename, min_degree, scatter_all, scatter_curves, plot_curves)
+
+def f_showAcc(filename, degree, scatter_all, scatter_curves, plot_curves):
+    reader = TrackFileReader()
+    reader.ReadFile(filename)
+
+    track = reader.GetTrack()    
+
+    segments = track.GetSegments()
+
+    dis = 0
+    acc = 0
+    acc2 = 0
+    curv=0   
+
+    for seg in segments:
+
+        fig = plt.figure(figsize=(55, 25))
+        fig.suptitle("CURVES", size="xx-large")
+        grid = plt.GridSpec(1, 1, wspace=0.5, hspace=0.5)
+        ax1 = fig.add_subplot(grid[0, 0])
+
+        c_count, intens, mid_p_curve, all_p_curve = seg.GetCurves(min_degree = degree, min_p_sep = 5)
+
+        puntos = seg.GetPoints()
+
+        # Show the whole route
+        eje_x = [( i.Longitude  ) for i in seg.GetPoints()]
+        eje_y = [( i.Latitude  ) for i in seg.GetPoints()]
+        ax1.plot(eje_x, eje_y, color = "blue")
+
+        # Build axes with the middle point of each curve
+        eje_x2 = []
+        eje_y2 = []
+        for i in mid_p_curve:
+            eje_x2.append( puntos[i].Longitude)
+            eje_y2.append( puntos[i].Latitude)
+        
+
+        # PAINT SMALL LINES SHOWING EACH CURVES
+        if plot_curves:
+            eje_x3 = []
+            eje_y3 = []
+            t=0
+            for i in range(0, len(all_p_curve)-2, 3):
+                i1,i2,i3 = all_p_curve[i][0], all_p_curve[i][1], all_p_curve[i][2]
+                eje_x3.append( (puntos[i1].Longitude, puntos[i2].Longitude,  puntos[i3].Longitude) )
+                eje_y3.append( (puntos[i1].Latitude, puntos[i2].Latitude,  puntos[i3].Latitude) )
+                ax1.plot(eje_x3[t], eje_y3[t], color = "red")
+                t=t+1
+
+        # Show some Segment info on the console
+        print("SEGMENTO")
+        print ("\tDist ->: ", round(seg.GetLength(),2), "m")
+        print ("\tAcc+ ->: ", round(seg.GetAccElevation(),2), "m")
+        print ("\tAcc- ->: ", round(seg.GetAccDescent(),2), "m")
+        print ("\tCurv ->: ", c_count, " curves")
+        dis += seg.GetLength()
+        acc += seg.GetAccElevation()
+        acc2 += seg.GetAccDescent()
+        curv += c_count
+        
+        # Scatter ALL points
+        if scatter_all:
+            ax1.scatter(eje_x, eje_y, color = "green")     
+        
+        # Scatter ONLY Middle POINTS of curves
+        if scatter_curves:
+            ax1.scatter(eje_x2, eje_y2, color = "red")     
+
+        plt.show()
+        
+        
+    plt.close()       
+
+
+    print ("\nTOTAL:")
+    print ("\tDist ->: ", round(dis,2), "m")
+    print ("\tAcc+ ->: ", round(acc,2), "m")
+    print ("\tAcc- ->: ", round(acc2,2), "m")
+    print ("\tCurv ->: ", curv, " curves")
+
+    print ("HOLA")
