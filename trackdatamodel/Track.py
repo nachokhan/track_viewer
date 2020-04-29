c1b3721fc8823ee445f33e44546cd8890c4afb33
Nachokhan <ignacio.rigoni@gmail.com>
2020-04-29 18:20:24 -0300

    (+) Author and Name in the track

--- trackdatamodel/Track.py
+++ trackdatamodel/Track.py
@@ -7,15 +7,25 @@ from trackdatamodel.GPSPoint import GPSPoint
 
 class Track:
     def __init__(self):
+        #Reality
         self.__segments = []
+        self.__author = None
+        self.__name = None
+
+        #Truth
         self.__northest_point = None
         self.__southest_point = None
         self.__westest_point = None
         self.__eastest_point = None
+
     
     def AddSegment(self, segment):
         """ Add a new segment to the track """
         self.__segments.append(segment)
+        
+    def GetSegments(self):
+        """ Returns all the segments in the track """
+        return self.__segments
     
     def GetLength(self):
         """ Returns the length of the whole Track """
@@ -24,12 +34,21 @@ class Track:
             d += segment.GetLength()
         return d
 
-    def GetSegments(self):
-        """ Returns all the segments in the track """
-        return self.__segments
+    def SetName(self, name):
+        self.__name = name
+    
+    def GetName(self):
+        return self.__name
+
+    def SetAuthor(self, author):
+        self.__author = author
+    
+    def GetAuthor(self):
+        return self.__author
+    
 
     def GetBoundaries(self):
-        """ Return Boundary Box as tuple (MinLon, MaxLon, MinLat, MaxLat) """
+        """ Returns Boundary Box as tuple (MinLon, MaxLon, MinLat, MaxLat) """
         minLon = self.__westest_point[0]
         maxLon = self.__eastest_point[0]
         minLat = self.__southest_point[1]
@@ -37,7 +56,7 @@ class Track:
         return (minLon, maxLon, minLat, maxLat)
 
     def GetExtremPoint(self, direction):
-
+        """ Returns the 'direction' extrem point. direcction is ('n','s','w' or 'e') """
         if not self.__northest_point:
             self.__calcExtremPoints()
         
