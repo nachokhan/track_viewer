6c6e03fa66c21d2bf112161955a8689b4f540b61
Nachokhan <ignacio.rigoni@gmail.com>
2020-04-29 18:22:10 -0300

    dsds

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from trackdatamodel.Segment import Segment
 import plottxt
 from Pruebas import Curvas_01, Curvas_02
+from Pruebas import GPXRead_01
 
 def f_plottxt():
     sys.argv.append("../track_analyzer/other/output/rincon2___/10/VIEW_05_03_250.txt")
@@ -11,12 +12,9 @@ def f_plottxt():
 
 if __name__ == "__main__":
     
-    #Curvas_02.main("./data/rincon1.txt", 50, scatter_all=False, scatter_curves=True, plot_curves=True)
-    Curvas_01.main()
+    #Curvas_02.main("./data/txt/rincon1.txt", 50, scatter_all=False, scatter_curves=True, plot_curves=True)
+    #Curvas_01.main()
+
+    GPXRead_01.main("./data/gpx/cristo2.gpx")
    
-    
-
-   
-
-
     
